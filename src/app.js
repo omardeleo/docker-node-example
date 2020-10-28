249dcae32027564bc1f91b59604819a2e7a4d072
Omar De Leo <omar.deleo@gmail.com>
2020-10-28 16:45:01 -0400

    Update

--- src/app.js
+++ src/app.js
@@ -6,49 +6,49 @@ new Vue({
     events: []
   },
 
-  ready: function () {
+  mounted: function () {
     this.fetchEvents();
   },
 
   methods: {
 
     fetchEvents: function () {
-      var events = [];
+      console.log("fetchEvents")
+
       this.$http.get('/api/events')
-        .success(function (events) {
-          this.$set('events', events);
-          console.log(events);
-        })
-        .error(function (err) {
+        .then(function (res) {
+          this.$set(this, 'events', res.body);
+          console.log(res.body);
+        }, function (err) {
           console.log(err);
-        });
+        })
     },
 
     addEvent: function () {
-      if (this.event.title.trim()) {
-        this.$http.post('/api/events', this.event)
-          .success(function (res) {
-            this.events.push(this.event);
-            console.log('Event added!');
-          })
-          .error(function (err) {
-            console.log(err);
-          });
-      }
+      // if (this.event.title.trim()) {
+      //   this.$http.post('/api/events', this.event)
+      //     .success(function (res) {
+      //       this.events.push(this.event);
+      //       console.log('Event added!');
+      //     })
+      //     .error(function (err) {
+      //       console.log(err);
+      //     });
+      // }
     },
 
     deleteEvent: function (id) {
-      if (confirm('Are you sure you want to delete this event?')) {        
-        this.$http.delete('api/events/' + id)
-          .success(function (res) {
-            console.log(res);
-            var index = this.events.find(x => x.id === id)
-            this.events.splice(index, 1);
-          })
-          .error(function (err) {
-            console.log(err);
-          });
-      }
+      // if (confirm('Are you sure you want to delete this event?')) {        
+      //   this.$http.delete('api/events/' + id)
+      //     .success(function (res) {
+      //       console.log(res);
+      //       var index = this.events.find(x => x.id === id)
+      //       this.events.splice(index, 1);
+      //     })
+      //     .error(function (err) {
+      //       console.log(err);
+      //     });
+      // }
     }
   }
 });
